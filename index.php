999cd9a682bf5c2c97ab0f4899d332757b86794d
Blizoman <xblizna00@stud.fit.vut.cz>
2026-02-09 11:22:11 +0100

    Changed lang

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
+<?php
+// Tento súbor zatiaľ obsahuje len HTML
+?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>
-            Nygga
-        </title>
+        <title>Nygga</title>
     </head>
-
     <body>
-        <h1 style="text-align: center;">
-            Bobson
-        </h1>
+        <h1 style="text-align: center;">Bobson</h1>
         <div style="text-align: center;">
             <button>Click me</button>
         </div>
-
     </body>
 </html>
