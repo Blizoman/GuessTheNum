617f648ba7024a76d52b4ce0ade8f605d24f3b83
Blizoman <xblizna00@stud.fit.vut.cz>
2026-02-13 11:04:59 +0100

    Added target picture and resolution of main page

--- index.php
+++ index.php
@@ -7,9 +7,19 @@
         <title>Nygga</title>
     </head>
     <body>
-        <h1 style="text-align: center;">Bobson</h1>
+        <h1 style="text-align: center;">Guess the number game</h1>
+
+        <span style="display: inline-block; width: 300px; height: 100px;"></span>
+
+        <form style="text-align: center;">
+            <input type="text" name="num" placeholder="Enter number">
+            <button type="submit">Submit</button>
+        </form>
+
+        <span style="display: inline-block; width: 300px; height: 100px;"></span>
+
         <div style="text-align: center;">
-            <button>Click me</button>
+            <img src="target.png" style="width: 400px; height: 300px; ">
         </div>
     </body>
 </html>
